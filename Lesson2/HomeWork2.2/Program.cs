db9e2dda13cd5a576eeb95adf04d841ffc513957
Baurzhan Abdykalykov <babdykalykov@yandex.kz>
2024-03-25 14:55:11 +0500

    ADDed HomeWork2.3

---
+++ Lesson2/HomeWork2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+//            |
+//            |
+//      2     |      1
+//            |
+// -----------------------
+//            |
+//      3     |      4
+//            |
+//            |
+
+Console.WriteLine("Ведите координату Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ведите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0) Console.WriteLine("Координатная четверть - 1");
+else if (x < 0 && y > 0) Console.WriteLine("оординатная четверть - 2");
+else if (x < 0 && y < 0) Console.WriteLine("оординатная четверть - 3");
+else if (x > 0 && y < 0) Console.WriteLine("оординатная четверть - 4");
+else Console.WriteLine("Ошибка!");
---
+++ Lesson2/HomeWork2.3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Ведите целое число:");
+int num = Convert.ToInt32(Console.ReadLine());
+int digit1 = num / 10;
+int digit2 = num % 10;
+int max = digit1 > digit2 ? digit1 : digit2;
+Console.WriteLine(max);
