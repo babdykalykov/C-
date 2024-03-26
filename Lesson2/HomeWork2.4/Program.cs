27aa863aa61c4ec89092df52f3ecf4c236fe0daa
Baurzhan Abdykalykov <babdykalykov@yandex.kz>
2024-03-26 10:33:23 +0500

    ADDed HomeWork2.4

---
+++ Lesson2/HomeWork2.4/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 4: Напишите программу, которая на вход принимает натуральное число N, а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string result = "";
+while (num > 0)
+{
+    // Добавление последней цифры к результату
+    result = num % 10 + "," + result;
+    num /= 10;
+}
+
+// Вывод результата
+Console.WriteLine(result.TrimEnd(','));
+
+
