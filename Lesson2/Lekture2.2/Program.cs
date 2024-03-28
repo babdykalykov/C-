f5c8ff0a6f7642ee50976be88d4f975b693d20b5
Baurzhan Abdykalykov <babdykalykov@yandex.kz>
2024-03-28 22:18:54 +0500

    ADDed_Lekture2

---
+++ Lesson2/Lekture2.2/Program.cs
@@ -0,0 +1,13 @@
+﻿// int n = 10;
+int[] arr = { 2, 3, 4, 5, 7, 6, 5, 4, 5, 6 };
+int i = 0;
+
+//while (i < n)
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    i = i + 1;
+}
---
+++ Lesson2/Lekture2.3/Program.cs
@@ -0,0 +1,36 @@
+﻿int n = 10;
+int[] array = { 2, 4, 17, 3, 9, 5, 23, 576, 54, 234 };
+int i = 0;
+int max = array[0];
+
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+    i = i + 1;
+}
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+
+Console.WriteLine(max);
+
+max = array[0];
+
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
